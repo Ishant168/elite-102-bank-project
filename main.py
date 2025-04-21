3f40ea32e99ffdba298b15ab9fbdf1f833fc2343
Ishwor Bastola <ishworbastola@Ishwors-MBP.lan>
2025-04-21 08:18:41 -0500

    Added basic banking app functionality

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello, World')
+def banking_menu():
+    print("/nPlease choose an option below")
+    print("1. Sign in")
+    print("2. login")
+    print("3. Deposit")
+    print("4. Withdraw")
+    print("5. Check your balance")
+    print("6.EXIT")
+
+def main():
+    
